2301c056bbf745fa9445652fd16001fa035bbc33
Konrad Perko <konradperko@gmail.com>
2020-02-05 15:05:10 +0100

    remove unnecessary code and put options alphabetically

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(expensesRouter)
 
-const router = express.Router()
-app.use(router)
-
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
